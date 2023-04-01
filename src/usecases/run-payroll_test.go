683490006005fc4a3a201565ac00b94a3c979f32
fbaptista <chico.baptista@gmail.com>
2023-04-01 11:33:17 -0300

    test: hourly with multiple timecards

--- src/usecases/run-payroll_test.go
+++ src/usecases/run-payroll_test.go
@@ -86,6 +86,37 @@ func TestRunPayrollOneHourlyEmployeeNoOvertimeNoDeductions(t *testing.T) {
 	}
 }
 
+func TestRunPayrollOneHourlyEmployeeMultipleTimecardsNoOvertimeNoDeductions(t *testing.T) {
+	var er EmployeeRepository
+	er = repositories.MakeInMemoryEmployeeRepository()
+
+	empId := 1
+
+	er.AddEmployee(entities.NewHourlyEmployee(empId, "Bob", "Home", 15.00))
+	PostTimecard{empId, time.Date(2023, 3, 30, 12, 30, 30, 100, time.Local), 6.00, er}.Execute()
+	PostTimecard{empId, time.Date(2023, 3, 29, 12, 30, 30, 100, time.Local), 8.00, er}.Execute()
+	PostTimecard{empId, time.Date(2023, 3, 28, 12, 30, 30, 100, time.Local), 8.00, er}.Execute()
+
+	tx := RunPayroll{time.Date(2023, 3, 31, 12, 30, 30, 100, time.Local), make(map[int]Paycheck), er}
+
+	tx.Execute()
+
+	pr := tx.Payroll
+
+	if len(pr) != 1 {
+		t.Fatalf("Failed to process Payroll, expected to have 1 entry")
+	}
+
+	pc, ok := tx.GetPaycheck(empId)
+	if !ok {
+		t.Fatalf(`Failed to process Payroll, expected to have paycheck for Employee %d.`, empId)
+	}
+
+	if diff := math.Abs(330.00 - pc.Amount); diff > 0.001 {
+		t.Fatalf(`Failed to process Payroll, want Paycheck Amount to be %f, got %f`, 330.00, pc.Amount)
+	}
+}
+
 func TestRunPayrollOneHourlyEmployeeWithOvertimeNoDeductions(t *testing.T) {
 	var er EmployeeRepository
 	er = repositories.MakeInMemoryEmployeeRepository()
